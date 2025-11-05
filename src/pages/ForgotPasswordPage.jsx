99b53bb876eec72d7f86095421f4c4382d90561d
spawngol31 <jbq0101@gmail.com>
2025-11-04 22:19:19 -0300

    fix forgotpasswordpage

--- src/pages/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage.jsx
@@ -1,6 +1,6 @@
 // src/pages/ForgotPasswordPage.jsx
 import React, { useState } from 'react';
-import axios from 'axios';
+import axiosInstance from '../api/axiosInstance';
 import { Link } from 'react-router-dom';
 
 function ForgotPasswordPage() {
